6cb774da299519e62f55cabc48edcec245f7ae93
Sandeep Kushwaha <sandeepkush327@gmail.com>
2019-08-21 13:01:06 +0530

    Intermediate Challenge IfElse_DecisionMaking1

---
+++ src/com/kushwahatechnologies/challenges/intermediate/IfElse_DecisionMaking1.java
@@ -0,0 +1,26 @@
+package com.kushwahatechnologies.challenges.intermediate;
+
+/**
+ * Intermediate Java Challenge #001
+ *
+ * 1. Very first take input for Number of Operation O. where 1 <= O <= 10
+ * 2. In Every Operation take two input N, M and check N!=M or N==M
+ *      according to operation print equals, lesser or greater
+ *      where -1000 <= N <= 1000 and -1000 <= M <= 1000
+ * Example:
+ *  Input:
+ *      2
+ *      7 9
+ *      2 2
+ *  Output:
+ *      lesser
+ *      equal
+ *
+ *
+ * */
+
+public class IfElse_DecisionMaking1 {
+    public static void main(String[] args) {
+        // code here...
+    }
+}
--- src/com/kushwahatechnologies/challenges/intermediate/readme.md
+++ src/com/kushwahatechnologies/challenges/intermediate/readme.md
@@ -2,4 +2,9 @@
 
 #### All challenges are following.
 
-    1...
+    
+| S. No.    | Challenge Name         |   Challenge Date    | Solution post Date                 |   Status   |
+| ---:      |        :---:           |        :---:        |        :---:                       |    :---:   |
+|   #001    | IFElse_DecisionMaking  |   August 21, 2019   |  August 24, 2019  __new__          | Running    |
+|           |                        |                     |                                    |            |
+|           |                        |                     |                                    |            |
