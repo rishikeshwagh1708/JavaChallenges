b76a6594e02c5ef90330454c604994e4e6ada683
Sandeep Kushwaha <sandeepkush327@gmail.com>
2019-09-09 16:26:34 +0530

    #003 and #004 Beginners Challenge Completed

---
+++ src/com/kushwahatechnologies/challenges/beginners/OperatorsInJava.java
@@ -0,0 +1,51 @@
+package com.kushwahatechnologies.challenges.beginners;
+
+/**
+ *  BEGINNERS JAVA CHALLENGE #003
+ *
+ *      Read the program and Guess the output.
+ *      Write your output in the multiline comment section.
+ *
+ * */
+
+public class OperatorsInJava {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = 30;
+        boolean a = false;
+
+        System.out.println(x + y * z - 2);
+        System.out.println(x - y / z + 2 * 4 > x + y - Math.pow(2, 8));
+        System.out.println(x + y * z - 2 <= 3 * x + 5 * y - z);
+        System.out.println(2 * x + y * 6 * z - 2 >= 3 * z ? ++x : a);
+        System.out.println(x + y * z - 2 % 2 != 2 * y % z ? "foo" : "boo");
+        System.out.println(x++ - 2 % 2 != 2 * y % z ? "loo" : "zoo");
+        System.out.println(x + y * z != 2 * y % z ? "foo" : "boo");
+        System.out.println(x-- + y * z* 'a' - 2 % 2 == 2 * y % z ? "got" : "tod");
+        System.out.println(x-- + y * z * 'a' == 2 * y % z ? "got" : "tod");
+        System.out.println(x + y * 'z' - 2 % 2 >= 2 * y % z ? "fin" : "bin");
+        System.out.println((x^y) < z ? "fine" : "ohh");
+
+        System.out.println(((x > y) ? (x > z) ? a : z : (y > z) ? y : z));
+    }
+
+    /**
+     * Output Console:
+     * -----------------------------
+     *  608
+     *  true
+     *  false
+     *  11
+     *  foo
+     *  loo
+     *  foo
+     *  tod
+     *  tod
+     *  fin
+     *  ohh
+     *  30
+     *  
+     *
+     * */
+}
--- src/com/kushwahatechnologies/challenges/beginners/readme.md
+++ src/com/kushwahatechnologies/challenges/beginners/readme.md
@@ -11,6 +11,8 @@
 | S. No.    | Challenge Name      |   Challenge Date    | Solution post Date                 |   Status   |
 | ---:      |        :---:        |        :---:        |        :---:                       |    :---:   |
 |   #001    |  VariablesInJava    |   August 14, 2019   |  August 17, 2019                   | Completed  |
-|   #002    |  StringConcatenation|   August 21, 2019   |  August 23, 2019 __new__           | Completed  |
+|   #002    |  StringConcatenation|   August 21, 2019   |  August 23, 2019                   | Completed  |
+|   #003    |  OperatorsInJava    |  September 6, 2019  |  September 9, 2019 __new__         | Completed  |
+|   #004    |  IfElseStatement    |  September 6, 2019  |  September 9, 2019 __new__         | Completed  |
 |           |                     |                     |                                    |            |
 |           |                     |                     |                                    |            |
