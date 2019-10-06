f550fbe5b84df2b280d465b71532fc7d70a3b95c
Sandeep Kushwaha <sandeepkush327@gmail.com>
2019-10-06 15:25:45 +0530

    #005 Beginners Challenge Solution

---
+++ src/com/kushwahatechnologies/challenges/beginners/SwitchStatement.java
@@ -0,0 +1,144 @@
+package com.kushwahatechnologies.challenges.beginners;
+
+/**
+ *  BEGINNERS JAVA CHALLENGE #005
+ *
+ *      Read the program care fully and write the code for Task 1 and 2:
+ *
+ *      Task 1: check the day string and return the weekDay
+ *              where Sunday = 1, Monday = 2, Tuesday = 3, Wednesday = 4
+ *                    Thursday = 5, Friday = 6, Saturday = 7 and for other string = -1
+ *
+ *      Task 2: calculate the result based on operands and Operators.
+ *              If there in not defined operator then return the Minimum Integer value.
+ *
+ *      NOTE: Use switch-case statement.
+ *            Syntax:
+ *              switch(testCase) {
+ *                  case test1:
+ *                        // code here...
+ *                  case test2:
+ *                        // code here...
+ *                  default:
+ *                        // code here...
+ *              }
+ *
+ *
+ **/
+
+public class SwitchStatement {
+
+    private static final String MINUS = "-";
+    private static final String PLUS = "+";
+    private static final String MULTIPLY = "*";
+    private static final String DIVIDE = "/";
+    private static final String MODULUS = "%";
+
+    /**
+     * Main method of the class which invoked automatically by the JVM.
+     *
+     */
+    public static void main(String[] args) {
+
+        // Task 1:
+        System.out.println(getWeekDay("Monday"));
+        System.out.println(getWeekDay("Friday"));
+        System.out.println(getWeekDay("October"));
+
+        // Task 2:
+        System.out.println(calculateInt(2564, 60934, PLUS));
+        System.out.println(calculateInt(27564, 6934, MINUS));
+        System.out.println(calculateInt(164, 34, MULTIPLY));
+        System.out.println(calculateInt(2564, 23, DIVIDE));
+        System.out.println(calculateInt(2564, 26, MODULUS));
+    }
+
+    /**
+     *  Method getWeekDay takes name of the Day of the Week,
+     *  and returns the dedicated number for the day.
+     *  If the given name is not the days of week then returns -1.
+     *
+     *  where
+     *  @param day indicates name of the Day of the Week.
+     *
+     */
+    private static int getWeekDay(String day) {
+        if (day == null || day.isEmpty()) {
+            return -1;
+        } else {
+            int weekDay = -1;
+
+            // Task 1,
+            day += day.toLowerCase();
+            switch (day) {
+                case "sunday":
+                    weekDay = 1;
+                    break;
+                case "monday":
+                    weekDay = 2;
+                    break;
+                case "tuesday":
+                    weekDay = 3;
+                    break;
+                case "wednesday":
+                    weekDay = 4;
+                    break;
+                case "thursday":
+                    weekDay = 5;
+                    break;
+                case "friday":
+                    weekDay = 6;
+                    break;
+                case "saturday":
+                    weekDay = 7;
+                    break;
+                default:
+                    break;
+            }
+
+            return weekDay;
+        }
+    }
+
+    /**
+     *  Method calculateInt takes three param and returns the calculation
+     *  based on the operator.
+     *  Tf there is not valid operator then it return Minimum Integer Value.
+     *
+     *  where
+     *  @param x is the first number
+     *  @param y is the second number
+     *  @param operator is the operator which provides the result.
+     *
+     */
+    private static int calculateInt(int x, int y, String operator) {
+        if (operator == null || operator.isEmpty()) {
+            return -1;
+        } else {
+            int result = Integer.MIN_VALUE;
+
+            // Task 2,
+            switch (operator) {
+                case MINUS:
+                    result = x - y;
+                    break;
+                case PLUS:
+                    result = x + y;
+                    break;
+                case MULTIPLY:
+                    result = x * y;
+                    break;
+                case DIVIDE:
+                    result = x / y;
+                    break;
+                case MODULUS:
+                    result = x % y;
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
